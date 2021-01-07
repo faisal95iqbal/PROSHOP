18a8f8f4c9a37a168d7ef21d94194cae929489e1
Faisal Iqbal <faisal95iqbal@users.noreply.github.com>
2021-01-08 01:12:06 +0500

    frontend setup, mongoose setup and models created

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express' 
+import products from './data/products.js'
+import connectDB from './config/db.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.listen( PORT, console.log( `server running in ${process.env.NODE_ENV} mode on ${PORT}` ) )
+
+app.get( '/api/products', ( req, res ) => {
+    res.json(products)
+} )
+
+app.get( '/api/products/:id', ( req, res ) => {
+    const product = products.find(p => p._id === req.params.id)
+    res.json(product)
+})
+
