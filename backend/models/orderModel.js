b1d183b23d1b0ebcf162da44ca6fad704e1e20a6
Faisal Iqbal <faisal95iqbal@users.noreply.github.com>
2021-01-12 23:59:43 +0500

    checkout process completed and paypal added

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -13,14 +13,14 @@ const orderSchema = mongoose.Schema( {
         price: { type: Number, required: true },
         product: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Product' },
     }],
-    shippingAdree: {
+    shippingAddress: {
         address: { type: String, required: true },
         city: { type: String, required: true },
         postalCode: { type: String, required: true },
         country: { type: String, required: true },
     },
     paymentMethod: {
-        type: String,
+        type: Object,
         required:true,
     },
     paymentResult: {
